7b2014c7fc17d34ce144730fb898303414fa854c
SebastianOG509 <seortizg@unal.edu.co>
2023-03-28 21:16:44 -0500

    Se creo la vista de Login con Vue y Tailwind

---
+++ UNRUTA_FrontEnd/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!--ESTA PARTE IRIA NUESTRO NAVEGADOR-->
+
+  <!--ESTA PARTE RENDERIZA LAS RUTAS-->
+  <RouterView />
+</template>
